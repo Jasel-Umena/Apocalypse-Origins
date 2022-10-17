4eb9f7c46ca3e4c1203d3ffa046c546a2de0e605
Jasel <jllopfan2@yahoo.es>
2022-10-17 13:44:15 +0200

    Refractor stuffs

---
+++ src/main/java/net/apocalypseorigins/mirror.java
@@ -0,0 +1,39 @@
+package net.apocalypseorigins;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class mirror implements ToolMaterial {
+    public static final mirror INSTANCE = new mirror();
+
+    @Override
+    public int getDurability() {
+        return 10;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 0;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 5;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 22;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofStacks(Items.GLASS.getDefaultStack());
+    }
+}
